cda974965a6468008f1d785a02e0e1274ce08f9e
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2025-10-13 20:51:27 +0300

    Add CustomException

---
+++ src/ShouldDo.Contracts/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿using LanguageExt.Common;
+using System.Net;
+
+namespace ShouldDo.Contracts.Exceptions;
+
+public class CustomException(string message, Error[]? errors, int? statusCode = null, string? type = null) : Exception(message)
+{
+    public Error[] Errors { get; } = errors ?? [];
+    public int StatusCode { get; } = statusCode ?? (int)HttpStatusCode.InternalServerError;
+    public string Type { get; } = type ?? "https://www.rfc-editor.org/rfc/rfc7231#section-6.6.1";
+}
