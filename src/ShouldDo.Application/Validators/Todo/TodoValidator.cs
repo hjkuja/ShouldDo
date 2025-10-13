ca846ffc5d4224f65af7d61d5818a1681f6d89bc
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2025-10-13 19:34:38 +0300

    Add comments

--- src/ShouldDo.Application/Validators/Todo/TodoValidator.cs
+++ src/ShouldDo.Application/Validators/Todo/TodoValidator.cs
@@ -4,11 +4,24 @@ using ShouldDo.Contracts.Requests.Todo;
 
 namespace ShouldDo.Application.Validators.Todo;
 
+/// <summary>
+/// Validates todo items.
+/// </summary>
 public class TodoValidator : IValidator<CreateTodoRequest>
 {
+    /// <summary>
+    /// Validates that a todo create request can be converted to a valid database entity.
+    /// </summary>
+    /// <param name="item">The requested todo to add.</param>
+    /// <returns>Array of <see cref="Error"/>.</returns>
     public Task<Error[]> ValidateAsync(CreateTodoRequest item)
     {
         List<Error> errors = [];
+
+        // Error.Add(<string>) adds an "expected" error,
+        // while Error.Add(<Exception>) adds an "unexpected" error.
+        // These can be useful while handling the operation results.
+
         if (string.IsNullOrWhiteSpace(item.Title))
         {
             errors.Add("Title is required.");
